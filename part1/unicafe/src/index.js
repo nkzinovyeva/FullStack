7a83ce130ab3a7122d411d8cacc0b0231fd222c0
Nataliya Zinovyeva <52114410+nkzinovyeva@users.noreply.github.com>
2021-02-15 14:07:24 +0200

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,22 +9,43 @@ const Button = (props) => (
   </button>
 )
 
+const Stats = (props) => {
+  return (
+    <div>
+      <h4>Statistics</h4>
+      <Display value={props.good} text="good"/>
+      <Display value={props.neutral} text="neutral"/>
+      <Display value={props.bad} text="bad"/>
+      <Display value={props.all} text="all"/>
+      <Display value={(props.good - props.bad) / props.all} text="average"/>
+      <Display value={(props.good * 100 / props.all) + ' %'} text="positive"/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   return (
     <div>
       <h3>Give feedback</h3>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h4>Statistics</h4>
-      <Display value={good} text="good"/>
-      <Display value={neutral} text="neutral"/>
-      <Display value={bad} text="bad"/>
+      <Button handleClick={() => (
+        setGood(good + 1), 
+        setAll(all + 1)
+        )} text="good" />
+      <Button handleClick={() => (
+        setNeutral(neutral + 1),
+        setAll(all + 1)
+        )} text="neutral" />
+      <Button handleClick={() => (
+        setBad(bad + 1),
+        setAll(all + 1)
+        )} text="bad" />
+      <Stats good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }
